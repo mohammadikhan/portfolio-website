134a47dbe828c6d8bb8c249d52479b0c63e85659
Mohammad Khan <mohammadikhan821@gmail.com>
2026-02-04 19:42:21 -0500

    Completed Home and Logo components for now

---
+++ src/components/Home/Logo/index.jsx
@@ -0,0 +1,59 @@
+import './index.scss'
+import MK from '../../../assets/images/profile.png'
+import { useEffect, useRef } from 'react'
+import gsap from 'gsap-trial'
+import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+
+const Logo = () => {
+  const bgRef = useRef()
+  const solidLogoRef = useRef()
+  const paraRef = useRef();
+
+  useEffect(() => {
+    gsap.registerPlugin(DrawSVGPlugin)
+
+    gsap
+      .timeline()
+      .to(bgRef.current, {
+        duration: 1,
+        opacity: 1,
+      })
+      
+
+    gsap.fromTo(
+      solidLogoRef.current,
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        delay: 4,
+        duration: 4,
+      }
+    )
+
+  }, [])
+
+
+  return (
+    <div className="logo-container" ref={bgRef}>
+      <img className="solid-logo" ref={solidLogoRef} src={MK}alt="MK"/>
+      <svg
+        width="559pt"
+        height="897pt"
+        version="1.0"
+        viewBox="0 0 559 897"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g
+          className="svg-container"
+          transform="translate(0 457) scale(.1 -.1)"
+          fill="none"
+        >
+        </g>
+      </svg>
+    </div>
+  )
+}
+
+export default Logo
---
+++ src/components/Home/index.jsx
@@ -0,0 +1,54 @@
+import M from "../../assets/images/Name.png"
+import { Link } from "react-router-dom";
+import './index.scss';
+import AnimatedLetters from "../AnimatedLetters";
+import { useEffect, useState } from "react";
+import Logo from "./Logo";
+
+
+const Home = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['o', 'h', 'a', 'm', 'm', 'a', 'd']
+    const jobArray = ['S', 'o', 'f', 't', 'w', 'a', 'r', 'e', ' ', 'E', 'n', 'g', 'i', 'n', 'e', 'e', 'r', ' ']
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+    
+        return () => clearTimeout(timer)
+    }, [])
+
+    return (
+        <>
+            <div className="container home-page">
+                <div className="text-zone">
+                    <h1>
+                    <span className={`${letterClass} _8`}>H</span>
+                    <span className={`${letterClass} _9`}>i,</span>    
+                    <br/>
+                    <span className={`${letterClass} _10`}>I</span>    
+                    <span className={`${letterClass} _11`}>'m,</span>    
+                    <img src={M} alt="developer"/>
+                    <AnimatedLetters letterClass={letterClass}
+                    strArray={nameArray}
+                    idx={12}
+                    />
+                    <br />
+                    <AnimatedLetters letterClass={letterClass}
+                    strArray={jobArray}
+                    idx={15}
+                    />
+                    </h1>
+                    <h2>Full-Stack Developer / AI Developer / Dev-Ops</h2>
+                    <h2>Carleton University Honours Bachelor of Computer Science (2019-2024)</h2>
+                    <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+                </div>
+                <Logo/>
+            </div>
+        </>
+        
+    )
+}
+export default Home;
